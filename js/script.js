2cc4808114939745b65b4649b2bc4f2d462c666d
MikhakMisaghian <MikhakMisaghian@users.noreply.github.com>
2015-03-25 11:35:36 -0700

    added changes to get articles from NYT based on the city, and wikipedia resources based on the city, changing the title and headers based on the address user searches for

--- js/script.js
+++ js/script.js
@@ -13,7 +13,57 @@ function loadData() {
 
     // load streetview
 
-    // YOUR CODE GOES HERE!
+    // Google maps streetview request
+    var streetStr = $("#street").val();
+    var cityStr = $("#city").val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address +'';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+    // NYTimes AJAZ request
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=0350e12e7c4272d52c6939345400db9b:8:71673637';
+    $.getJSON(nytimesUrl, function(data) {
+        // console.log(data);
+        $nytHeaderElem.text('New York Times Articles About '+ cityStr);
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">' +
+                '<a href="' + article.web_url + '">' + article.headline.main + '</a>' +
+                '<p>' + article.snippet + '</p>' +
+                '</li>');
+        }
+    }).error(function(e){
+        $nytHeaderElem.text('New York Times Articles could not be loaded');
+    });
+
+
+    // Wikipedia API AJAX request
+    var wikiUrl = 'http://en.wikiasdfasdfasdfpedia.org/w/api.php?action=opensearch&search=' + cityStr +
+    '&format=json&callback=wikiCallback';
+
+    var wikiRequestTimeout = setTimeout(function() {
+        $wikiElem.text("failed to get the wikipedia resources");
+    }, 8000);
+
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        // jsonp: "callback",
+        success: function(response) {
+            var articleList = response[1];
+            for (var i = 0; i < articleList.length; i++) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+            };
+            clearTimeout(wikiRequestTimeout);
+        }
+
+    });
 
     return false;
 };
